4767bce7818a757e94874a45a4a71abfde4f12bb
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-15 14:22:04 +0000

    docs: apply formatting

--- index.ts
+++ index.ts
@@ -14,6 +14,7 @@ import {JsDate} from "./js-date";
 /** An instant in time, represented as a date and time in the Gregorian
  * Calendar, UTC. */
 export interface Timestamp {
+    /** Type discriminator identifying the object as a `Timestamp`. */
     readonly type: "timestamp";
 
     /** The year.
@@ -21,12 +22,12 @@ export interface Timestamp {
      * Should be an integer.
      *
      * Positive values represent years in the Common Era (CE/AD). For example,
-     * 2021 represents 2021 CE, the year this module was first published to
+     * `2021` represents 2021 CE, the year this module was first published to
      * npm.
      *
      * Negative values or zero represent years before the Common Era (BCE/BC).
-     * Zero represents 1 BCE, -1 represents 2 BCE, -2 represents BCE, and so
-     * on.
+     * Zero represents 1 BCE, `-1` represents 2 BCE, `-2` represents BCE, and
+     * so on.
      *
      * Note that there is no year zero in the Gregorian Calendar. The year 1
      * BCE was immediately followed by 1 CE.
@@ -34,43 +35,43 @@ export interface Timestamp {
      * @default 1 */
     readonly year: number;
 
-    /** The month of the year. Should be an integer in the range 1-12.
+    /** The month of the year. Should be an integer in the range `1`-`12`.
      *
      * @default 1 */
     readonly month: number;
 
-    /** The day of the month. Should be an integer in the range 1-31.
+    /** The day of the month. Should be an integer in the range `1`-`31`.
      *
      * @default 1 */
     readonly day: number;
 
     /** The hours component of the time of day. Should be an integer in the
-     * range 0-23.
+     * range `0`-`23`.
      *
      * @default 0 */
     readonly hours: number;
 
     /** The minutes component of the time of day. Should be an integer in the
-     * range 0-59.
+     * range `0`-`59`.
      *
      * @default 0 */
     readonly minutes: number;
 
-    /** The seconds component of the time of day. Should be in the range 0-60,
-     * inclusive of 0 but exclusive of 60. May be fractional to represent an
+    /** The seconds component of the time of day. Should be in the range `0`-`60`,
+     * inclusive of `0` but exclusive of `60`. May be fractional to represent an
      * instant in time with sub-second accuracy.
      *
      * @default 0 */
     readonly seconds: number;
 }
 
-/** Options for creating a Timestamp.
+/** Options for creating a `Timestamp`.
  *
  * An instance of {@link Timestamp} may always be used in place of
- * TimestampOptions. */
+ * `TimestampOptions`. */
 export type TimestampOptions = Partial<Timestamp>;
 
-/** Tests if the specified value is a Timestamp. */
+/** Tests if the specified value is a `Timestamp`. */
 export function isTimestamp(value: unknown): value is Timestamp {
     return (
         typeof value === "object" &&
@@ -92,7 +93,7 @@ export function isTimestamp(value: unknown): value is Timestamp {
     );
 }
 
-/** Creates a Timestamp with the specified options.
+/** Creates a {@link Timestamp} with the specified options.
  *
  * If any numeric components are unspecified, they default to zero.
  *
@@ -113,7 +114,7 @@ export function fromJsDate(date: JsDate): Timestamp {
     });
 }
 
-/** Creates a Timestamp with the specified options.
+/** Creates a {@link Timestamp} with the specified options.
  *
  * If any numeric components are unspecified, they default to zero.
  *
@@ -121,8 +122,8 @@ export function fromJsDate(date: JsDate): Timestamp {
  * the resulting Timestamp will be normalized. */
 export const normalize = timestamp;
 
-/** Converts the specified Timestamp to a count of seconds since the reference
- * Timestamp of midnight on the morning of 1st January, 1 CE. */
+/** Converts the specified {@link Timestamp} to a count of seconds since the
+ * reference Timestamp of midnight on the morning of 1st January, 1 CE. */
 export function toReferenceSeconds(timestamp: TimestampOptions): number {
     const year = timestamp.year ?? 1;
     const month = timestamp.month ?? 1;
@@ -137,8 +138,9 @@ export function toReferenceSeconds(timestamp: TimestampOptions): number {
     );
 }
 
-/** Creates a Timestamp corresponding to the specified count of seconds since
- * the reference Timestamp of midnight on the morning of 1st January, 1 CE. */
+/** Creates a {@link Timestamp} corresponding to the specified count of seconds
+ * since the reference Timestamp of midnight on the morning of 1st January,
+ * 1 CE. */
 export function fromReferenceSeconds(referenceSeconds: number): Timestamp {
     const referenceDays = Math.floor(referenceSeconds / 86400);
     const {year, month, day} = fromReferenceDays(referenceDays);
@@ -148,27 +150,27 @@ export function fromReferenceSeconds(referenceSeconds: number): Timestamp {
     return {type: "timestamp", year, month, day, hours, minutes, seconds};
 }
 
-/** Tests if two Timestamps are equal. */
+/** Tests if two {@link Timestamp}s are equal. */
 export function equal(a: TimestampOptions, b: TimestampOptions): boolean {
     return toReferenceSeconds(a) === toReferenceSeconds(b);
 }
 
-/** Tests if two Timestamps are equal. */
+/** Tests if two {@link Timestamp}s are equal. */
 export function equalFn(b: TimestampOptions): (a: TimestampOptions) => boolean {
     return a => equal(a, b);
 }
 
-/** Tests if two Timestamps are not equal. */
+/** Tests if two {@link Timestamp}s are not equal. */
 export function notEqual(a: TimestampOptions, b: TimestampOptions): boolean {
     return toReferenceSeconds(a) !== toReferenceSeconds(b);
 }
 
-/** Tests if two Timestamps are not equal. */
+/** Tests if two {@link Timestamp}s are not equal. */
 export function notEqualFn(b: TimestampOptions): (a: TimestampOptions) => boolean {
     return a => notEqual(a, b);
 }
 
-/** Compares two Timestamps. */
+/** Compares two {@link Timestamp}s. */
 export const compare: Comparator<TimestampOptions> = (a, b) => {
     const as = toReferenceSeconds(a);
     const bs = toReferenceSeconds(b);
@@ -184,57 +186,60 @@ export const compare: Comparator<TimestampOptions> = (a, b) => {
     }
 };
 
-/** Compares two Timestamps. */
+/** Compares two {@link Timestamp}s. */
 export function compareFn(b: TimestampOptions): (a: TimestampOptions) => Comparison {
     return a => compare(a, b);
 }
 
-/** Tests if the Timestamp a is before the Timestamp b. */
+/** Tests if the {@link Timestamp} `a` is before the {@link Timestamp} `b`. */
 export function before(a: TimestampOptions, b: TimestampOptions): boolean {
     return toReferenceSeconds(a) < toReferenceSeconds(b);
 }
 
-/** Tests if the Timestamp a is before the Timestamp b. */
+/** Tests if the {@link Timestamp} `a` is before the {@link Timestamp} `b`. */
 export function beforeFn(b: TimestampOptions): (a: TimestampOptions) => boolean {
     return a => before(a, b);
 }
 
-/** Tests if the Timestamp a is before or equal to the Timestamp b. */
+/** Tests if the {@link Timestamp} `a` is before or equal to the
+ * {@link Timestamp} `b`. */
 export function beforeOrEqual(a: TimestampOptions, b: TimestampOptions): boolean {
     return toReferenceSeconds(a) <= toReferenceSeconds(b);
 }
 
-/** Tests if the Timestamp a is before or equal to the Timestamp b. */
+/** Tests if the {@link Timestamp} `a` is before or equal to the
+ * {@link Timestamp} `b`. */
 export function beforeOrEqualFn(b: TimestampOptions): (a: TimestampOptions) => boolean {
     return a => beforeOrEqual(a, b);
 }
 
-/** Tests if the Timestamp a is after the Timestamp b. */
+/** Tests if the {@link Timestamp} `a` is after the {@link Timestamp} `b`. */
 export function after(a: TimestampOptions, b: TimestampOptions): boolean {
     return toReferenceSeconds(a) > toReferenceSeconds(b);
 }
 
-/** Tests if the Timestamp a is after the Timestamp b. */
+/** Tests if the {@link Timestamp} `a` is after the {@link Timestamp} `b`. */
 export function afterFn(b: TimestampOptions): (a: TimestampOptions) => boolean {
     return a => after(a, b);
 }
 
-/** Tests if the Timestamp a is after or equal to the Timestamp b. */
+/** Tests if the {@link Timestamp} `a` is after or equal to the
+ * {@link Timestamp} `b`. */
 export function afterOrEqual(a: TimestampOptions, b: TimestampOptions): boolean {
     return toReferenceSeconds(a) >= toReferenceSeconds(b);
 }
 
-/** Tests if the Timestamp a is after or equal to the Timestamp b. */
+/** Tests if the {@link Timestamp} `a` is after or equal to the {@link Timestamp} `b`. */
 export function afterOrEqualFn(b: TimestampOptions): (a: TimestampOptions) => boolean {
     return a => afterOrEqual(a, b);
 }
 
-/** Returns the earliest of the specified Timestamps. */
+/** Returns the earliest of the specified {@link Timestamp}s. */
 export function earliest<T extends TimestampOptions>(timestamps: Iterable<T>): Timestamp | null {
     return mapNullable(minimum(map(timestamps, toReferenceSeconds)), fromReferenceSeconds);
 }
 
-/** Returns the earliest of the specified Timestamps. */
+/** Returns the earliest of the specified {@link Timestamp}s. */
 export function earliestFn(b: TimestampOptions): (a: TimestampOptions) => Timestamp {
     const bs = toReferenceSeconds(b);
     return a => {
@@ -243,12 +248,12 @@ export function earliestFn(b: TimestampOptions): (a: TimestampOptions) => Timest
     };
 }
 
-/** Returns the latest of the specified Timestamps. */
+/** Returns the latest of the specified {@link Timestamp}s. */
 export function latest<T extends TimestampOptions>(timestamps: Iterable<T>): Timestamp | null {
     return mapNullable(maximum(map(timestamps, toReferenceSeconds)), fromReferenceSeconds);
 }
 
-/** Returns the latest of the specified Timestamps. */
+/** Returns the latest of the specified {@link Timestamp}s. */
 export function latestFn(b: TimestampOptions): (a: TimestampOptions) => Timestamp {
     const bs = toReferenceSeconds(b);
     return a => {
@@ -257,12 +262,12 @@ export function latestFn(b: TimestampOptions): (a: TimestampOptions) => Timestam
     };
 }
 
-/** Creates a Timestamp of the current time and date. */
+/** Creates a {@link Timestamp} of the current time and date. */
 export function now(): Timestamp {
     return fromJsDate(new JsDate());
 }
 
-/** Parses a Timestamp from text in ISO 8601 format.
+/** Parses a {@link Timestamp} from text in ISO 8601 format.
  *
  * The ISO 8601 text must specify a time zone offset, which should usually be
  * `Z` for UTC. If any other offset is specified then the date and time will
@@ -295,8 +300,8 @@ export function parseIso8601(text: string): Timestamp | null {
     return timestamp({year, month, day, hours, minutes, seconds});
 }
 
-/** Formats the specified Timestamp as IS0 8601 extended, rounded down to the
- * next lower second e.g. `2021-05-01T11:57:23Z`.
+/** Formats the specified {@link Timestamp} as IS0 8601 extended, rounded down
+ * to the next lower second e.g. `2021-05-01T11:57:23Z`.
  *
  * For other formats, see @softwareventures/format-timestamp. */
 export const formatIso8601 = format.iso8601;
