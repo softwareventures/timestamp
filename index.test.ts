449a80260e096bb4f5ef206f5c08b1a23478748b
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-16 05:03:48 +0000

    test(parseIso8601): update for changes to TimestampOptions

--- index.test.ts
+++ index.test.ts
@@ -30,7 +30,10 @@ test("parseIso8601", t => {
         parseIso8601("2021-04-30T13:11:27.245Z"),
         timestamp({year: 2021, month: 4, day: 30, hours: 13, minutes: 11, seconds: 27.245})
     );
-    t.deepEqual(parseIso8601("10000-01-01T00:00:00Z"), timestamp({year: 10000}));
+    t.deepEqual(
+        parseIso8601("10000-01-01T00:00:00Z"),
+        timestamp({year: 10000, month: 1, day: 1, hours: 0})
+    );
     t.deepEqual(
         parseIso8601("1994-11-05T08:15:30-05:00"),
         timestamp({
